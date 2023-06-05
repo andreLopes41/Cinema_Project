399ddf64a01bb4fc0092a9d5236a3eda9e4e2037
André Lopes <111528571+Andre-Davi-Lopes@users.noreply.github.com>
2023-06-05 09:02:44 -0300

    .

--- CinemaSFX/dbconfig.php
+++
@@ -1,9 +0,0 @@
-<?php
-    
-    $dbHost = 'LocalHost';
-    $dbUsername = 'root';
-    $dbPassword = '';
-    $dbName = 'cinema';
-
-    $conexao = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
-?>
